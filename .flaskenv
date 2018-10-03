215d50a9cadb36187d70b6bd3a8f2bc2eb04778a
v1b3m <vibenjamin6@gmail.com>
2018-10-03 22:35:59 +0300

    Add Email Support

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,5 @@
 FLASK_APP=microblog.py
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=sheldonflask1.0@gmail.com
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME =os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['vibenjamin6@gmail.com']
+    POSTS_PER_PAGE = 25
